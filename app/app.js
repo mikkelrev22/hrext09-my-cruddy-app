66042c0e5876cf199b5ad01f3156c0163ab5ca55
Michael Thompson <mikkelrev@Michaels-MacBook-Air.local>
2019-05-21 15:41:54 -0700

    working on adding 'eat' and 'buy' functions

--- app/app.js
+++ app/app.js
@@ -13,6 +13,8 @@
 
 //localStorage interaction function
 //get item
+
+
 var getItem = function(key) {
   return window.localStorage.getItem(key);
 }
@@ -42,6 +44,24 @@ var keyExists = function(key) {
   return currentValue !== null;
 }
 
+var addText = function(object) {
+  refreshDiv();
+  for (let key in object) {
+    if (typeof object[key] !== 'function' && key !== 'length') {
+    $(".text-container").append(`<div class="food-name"> ${key}<span class="food-quantity"> ${object[key]}</span> <button class="eat-button"type="button">Eat</button> <button class="buy-button"type="button">Buy</button></div>`)
+    console.log(object)
+  }
+  }
+}
+
+let refreshDiv = function() {
+ 
+  return $('.text-container').html('')
+ }
+
+let eat = function(key, value) {
+  return window.localStorage.setItem(key, value -1)
+}
 
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
@@ -52,11 +72,15 @@ $(document).ready(function() {
 
     var currentKey = $("#keyInput").val();
     var currentValue = $("#valueInput").val();
+    //let $currentValue = $(`<div class="food-name"> ${currentKey}<span class="food-quantity"> ${currentValue}</span> <button class="eat-button"type="button">Eat</button> <button class="buy-button"type="button">Buy</button></div>`)
     if (keyExists(currentKey)) {
       //current key exists, do something error-handle-y
     } else {
       createItem(currentKey, currentValue);
-    }
+      addText(localStorage);
+      }
+      
+    })
   });
 
   $('#updateButton').click(function(event) {
@@ -66,6 +90,7 @@ $(document).ready(function() {
     var currentValue = $("#valueInput").val();
     if (keyExists(currentKey)) {
       updateItem(currentKey, currentValue);
+      addText(localStorage);
     } else {
       //current key doesnt exist, do stuff
     }
@@ -76,10 +101,20 @@ $(document).ready(function() {
     var currentValue = $("#valueInput").val();
     if (keyExists(currentKey)) {
       deleteItem(currentKey, currentValue);
+      addText(localStorage);
     }
   });
   $('#clearAll').click(function(event) {
     clearEverything();
+    $('.text-container').text('')
   });
+  $('.eat-button').click(function() {
+    event.preventDefault();
+    console.log('it worked');
+    // if ($('.food-name').val() === localStorage.currentKey) {
+    //   eat(currentKey, currentValue)
+    //   addText(localStorage);
+    });
+  // }
 
-});
+
