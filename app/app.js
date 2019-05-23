d071a99b2cac6df28d8d837550ad3ce09d7d0c50
Michael Thompson <mikkelrev@Michaels-MacBook-Air.local>
2019-05-23 15:53:34 -0700

    getting closer to completing; may work on fixing minor bugs, edge cases

--- app/app.js
+++ app/app.js
@@ -48,7 +48,7 @@ var addText = function(object) {
   refreshDiv();
   for (let key in object) {
     if (typeof object[key] !== 'function' && key !== 'length') {
-    $(".text-container").append(`<div class="food-name"> ${key}<span class="food-quantity"> ${object[key]}</span> <button class="eat-button"type="button">Eat</button> <button class="buy-button"type="button">Buy</button></div>`)
+    $(".text-container").append(`<div class="food-name"> ${key}<span class="food-quantity"> ${object[key]}</span> <button class="eat-button"id="${key}"type="button">Eat</button> <button class="buy-button"id="${key}"type="button">Buy</button></div>`)
     console.log(object)
   }
   }
@@ -60,9 +60,14 @@ let refreshDiv = function() {
  }
 
 let eat = function(key, value) {
-  return window.localStorage.setItem(key, value -1)
+  
+  return window.localStorage.setItem(key, value- 1)
+
 }
 
+let buy = function(key, value) {
+  return window.localStorage.setItem(key, +value+ 1)
+}
 ///////////////////////////////////////////
 //event handlers for the buttons and ... possibly the inputboxes
   //preventdefault on button clicks
@@ -72,15 +77,35 @@ $(document).ready(function() {
 
     var currentKey = $("#keyInput").val();
     var currentValue = $("#valueInput").val();
-    //let $currentValue = $(`<div class="food-name"> ${currentKey}<span class="food-quantity"> ${currentValue}</span> <button class="eat-button"type="button">Eat</button> <button class="buy-button"type="button">Buy</button></div>`)
     if (keyExists(currentKey)) {
-      //current key exists, do something error-handle-y
-    } else {
+      alert('You already have an item named this!')
+      //add something here to prevent value from changing
+    } 
+    if (currentKey === undefined|| currentKey === '') {
+      alert('Remember to specify an item!')
+    }
+    else {
       createItem(currentKey, currentValue);
       addText(localStorage);
       }
       
     })
+   $('body').on('click', '.eat-button', function() {
+    //console.log(this.id);
+    if (localStorage[this.id] > 0) {
+    eat(this.id, localStorage[this.id]);
+    addText(localStorage);
+  }
+    // else if (localStorage[this.id] === 0){
+    //   console.log('it worked')
+    //   alert(`Looks like you are all out of ${this.id}!`)
+    // }
+    });
+   $('body').on('click', '.buy-button', function() {
+    //console.log(this.id);
+    buy(this.id, localStorage[this.id]);
+    addText(localStorage);
+   })
   });
 
   $('#updateButton').click(function(event) {
@@ -92,7 +117,7 @@ $(document).ready(function() {
       updateItem(currentKey, currentValue);
       addText(localStorage);
     } else {
-      //current key doesnt exist, do stuff
+      alert('Sorry! That item is not in your pantry yet!')
     }
   });
   $('#deleteButton').click(function(event) {
@@ -108,13 +133,6 @@ $(document).ready(function() {
     clearEverything();
     $('.text-container').text('')
   });
-  $('.eat-button').click(function() {
-    event.preventDefault();
-    console.log('it worked');
-    // if ($('.food-name').val() === localStorage.currentKey) {
-    //   eat(currentKey, currentValue)
-    //   addText(localStorage);
-    });
-  // }
+ 
 
 
